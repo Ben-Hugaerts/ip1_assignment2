ca64487ca66a240a12809917ec028db687fcdae7
Ben Hugaerts <ben.hugaerts@live.be>
2023-05-07 13:49:33 +0200

    Updated comments

--- charging_schedule.py
+++ charging_schedule.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 time_windows = True
 ##########################################
 
+# Make lists with the dates when packages need to be delivered and when vehicles have to be charged
 start_date = datetime.strptime('19/07/2022', '%d/%m/%Y')
 end_date = datetime.strptime('26/08/2022', '%d/%m/%Y')
 
@@ -27,6 +28,7 @@ while day_before_start_date <= day_before_end_date:
 locations_dataframe = pd.read_csv('locations.csv')
 cars_dataframe = pd.read_csv('packages.csv')
 
+# Determine the amount of locations that need to be visited on each day
 lengths =[]
 
 for date in date_list:
@@ -38,6 +40,9 @@ for date in date_list:
     location_ids = aggregated_volumes_packages_dataframe.index[:].tolist()
     lengths.append(len(location_ids))
 
+# Determine the amount of vehicles necessary for a certain amount of locations
+# Functions are fitted for this to results of the CVRP 8h and CVRP with time windows of 12h,
+# both with service time of 2min
 cars = []
 if time_windows == False:
     for i in range(len(lengths)):
@@ -50,86 +55,85 @@ else:
         if lengths[i] == 0:
             cars[i] = 0
 
-# Create a model
+# Create a model which minimizes the amount of chargers needed,
+# while maximizing the amount of charged vehicles at the depot
 m = Model("charging_schedule")
 
-# Create a variable for the maximum production per day
-max_prod = m.addVar(vtype=GRB.CONTINUOUS, name="max_prod")
+# Create a variable for the maximum chargers needed per day
+max_chargers = m.addVar(vtype=GRB.CONTINUOUS, name="max_chargers")
 
-# Create a list of variables for the production per day
-prod = []
+# Create a list of variables for the amount of vehicles charged each day
+charg = []
 for i in range(len(cars)):
-    prod.append(m.addVar(vtype=GRB.INTEGER, name=f"prod_{i+1}"))
+    charg.append(m.addVar(vtype=GRB.INTEGER, name=f"charg_{i+1}"))
 
-# Create a list of variables for the inventory per day
-inv = []
+# Create a list of variables for the vehicles which remain charged at the depot each day
+dep = []
 for i in range(len(cars)):
-    inv.append(m.addVar(vtype=GRB.INTEGER, name=f"inv_{i+1}"))
+    dep.append(m.addVar(vtype=GRB.INTEGER, name=f"dep_{i+1}"))
 
-# Create a variable for the total number of cars in inventory/depot
-total_cars_in_inv = m.addVar(vtype=GRB.INTEGER, name="total_cars_in_inv")
+# Create a variable for the total number of vehicles that remain charged at the depot over the whole period
+total_cars_in_dep = m.addVar(vtype=GRB.INTEGER, name="total_cars_in_dep")
 
-# Create a variable for the total number of decharged cars in inventory/depot
-total_cars_in_inv_d = m.addVar(vtype=GRB.INTEGER, name="total_cars_in_inv_d")
-
+# Add a constraint that prohibits that vehicles are charged which were already fully charged earlier
 for i in range(len(cars)):
-    m.addConstr(prod[i] + inv[i-1] <= max(cars))
+    m.addConstr(charg[i] + dep[i-1] <= max(cars))
 
-# Add a constraint to calculate the total number of cars in inventory/depot
-m.addConstr(total_cars_in_inv == quicksum(inv))
+# Add a constraint to calculate the total number of vehicles fully charged at the depot
+m.addConstr(total_cars_in_dep == quicksum(dep))
 
-# Add a constraint to calculate the total number of cars in inventory/depot
-m.addConstr(total_cars_in_inv_d == max(cars)*len(cars)-quicksum(inv)-quicksum(cars))
+# Set the objective to minimize the maximum amount of chargers used on a day
+# A second term is added to maximize the amount of charged vehicles at the depot
+m.setObjective(max_chargers - total_cars_in_dep/len(cars), GRB.MINIMIZE)
 
-# Set the objective to minimize the maximum production per day
-m.setObjective(max_prod - total_cars_in_inv/len(cars), GRB.MINIMIZE)
-# m.setObjective(max_prod, GRB.MINIMIZE)
-
-# Add a constraint that the production per day must be less than or equal to the maximum production per day
+# Add a constraint that the chargers used on a day must be less than or equal to the maximum chargers used on a day
 for i in range(len(cars)):
-    m.addConstr(prod[i] <= max_prod)
+    m.addConstr(charg[i] <= max_chargers)
 
-# Add a constraint that the inventory per day must be equal to the previous inventory plus the production minus the cars
+# Add a constraint that the vehicles that remain fully charged at the depot each day must be equal to the vehicles that
+# remained fully charged at the depot the previous day + the extra vehicles that are charged for that day - the amount
+# of vehicles used that day
 for i in range(len(cars)):
     if i == 0:
-        m.addConstr(inv[i] == prod[i] - cars[i])
+        m.addConstr(dep[i] == charg[i] - cars[i])
     else:
-        m.addConstr(inv[i] == inv[i-1] + prod[i] - cars[i])
+        m.addConstr(dep[i] == dep[i-1] + charg[i] - cars[i])
 
-# Add a constraint that the inventory per day must be non-negative
+# Add a constraint that the amount of vehicles that remain fully charged at the depot each day must be non-negative
 for i in range(len(cars)):
-    m.addConstr(inv[i] >= 0)
+    m.addConstr(dep[i] >= 0)
 
 # Optimize the model
 m.optimize()
 
+# Print the result
 # Define the column widths
 date_width = 15
 charge_width = 20
-inv_width = 35
+dep_width = 35
 demand_width = 20
 
 # Print the headers
-print(f"{'Date':<{date_width}}{'Charging':<{charge_width}}{'Vehicles at depot the next day':<{inv_width}}{'Vehicles needed the next day':<{demand_width}}")
-print("-" * (date_width + charge_width + inv_width + demand_width + 3))
+print(f"{'Date':<{date_width}}{'Charging':<{charge_width}}{'Vehicles at depot the next day':<{dep_width}}{'Vehicles needed the next day':<{demand_width}}")
+print("-" * (date_width + charge_width + dep_width + demand_width + 3))
 
 # Print the data
 for i in range(len(cars)):
-    charge_str = f"Charge {int(prod[i].x)} vehicles "
-    inv_str = f"{int(inv[i].x)} charged, {int(max(cars) - inv[i].x - cars[i])} decharged "
+    charge_str = f"Charge {int(charg[i].x)} vehicles "
+    dep_str = f"{int(dep[i].x)} charged, {int(max(cars) - dep[i].x - cars[i])} decharged "
     demand_str = f"{int(cars[i])} vehicles"
     print(
-        f"{charge_date_list[i]:<{date_width}}{charge_str:<{charge_width}}{inv_str:<{inv_width}}{demand_str:<{demand_width}}")
+        f"{charge_date_list[i]:<{date_width}}{charge_str:<{charge_width}}{dep_str:<{dep_width}}{demand_str:<{demand_width}}")
 
-print(f"\nAmount of chargers needed: {int(max_prod.x)}")
+print(f"\nAmount of chargers needed: {int(max_chargers.x)}")
 print("")
 
 
 # This is just a test scenario to show that a vehicle doesn't necessary remain too long in the same state:
 
 # Convert schedule results to lists
-inv_values = [v.X for v in inv]
-prod_values = [p.X for p in prod]
+dep_values = [v.X for v in dep]
+charg_values = [p.X for p in charg]
 
 # List containing usage of each vehicle for every day
 states = [['d'] * max(cars)] * (len(cars) + 1)
@@ -158,7 +162,7 @@ for i in range(1, len(states)):
         insert_index = (insert_index + 1) % len(new_states[i])
 
     if demand_assigned == demand:
-        demand_c = inv_values[i-1]
+        demand_c = dep_values[i-1]
         if demand_c > 0:
             if 'c' in new_states[i - 1]:  # use the new list for the previous day's state
                 index_c = new_states[i - 1].index('c')
